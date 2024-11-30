f38e9e6e121e5b0259d80c28a6a6547c6a2f4a30
Valentina-7-77 <natandr777@gmail.com>
2024-12-01 01:22:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+   # Переменные разных типов данных
+immutable_var = 1, 2, 'Зима', 'Весна', True
+print(immutable_var)
+   # Изменение значений переменных
+ # immutable_var [2] = 'Осень'
+ # print(immutable_var)
+   # Создание изменяемых структур данных
+mutable_list = [1, 2, 'Зима', 'Весна', True]
+print(mutable_list)
+mutable_list [0] = 3
+mutable_list [1] = 4
+mutable_list [2] = 'Осень'
+mutable_list [3] = 'Лето'
+mutable_list [4] = False
+print(mutable_list)
